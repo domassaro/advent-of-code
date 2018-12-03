6a8a08ef670603e79989d94134edc78e547831e1
Deirdre Massaro <deirdre.massaro@Leafly.com>
2018-12-02 20:17:31 -0800

    Part One of Day 2

--- 2018/day1/day1.js
+++ 2018/day1/day1.js
@@ -1,27 +1,23 @@
-var fs = require('fs'),
+let fs = require('fs'),
     path = require('path'),    
     filePath = path.join(__dirname, 'input.txt');
 
-
-
 let dataFrequency = fs.readFileSync(filePath, {encoding: 'utf-8'});
-
-var lines = dataFrequency.split('\n');
+let lines = dataFrequency.split('\n');
 let frequencyCalculation = 0;
 let allFrequencies = new Set ([]);
-
 let found = false;
+
 while (!found) {
     for (let i = 0; i < lines.length; i++) {
         newFrequencyValue = parseInt(lines[i]);
         frequencyCalculation += newFrequencyValue;
 
         if (allFrequencies.has(frequencyCalculation)) {
-            console.log("This is it" + frequencyCalculation);
+            console.log("The first frequency my device reaches twice is " + frequencyCalculation);
             found = true;
             break;
         }
-        
         allFrequencies.add(frequencyCalculation);    
     }
 
---
+++ 2018/day2/partOne.js
@@ -0,0 +1,30 @@
+let fs = require('fs'),
+    path = require('path'),    
+    filePath = path.join(__dirname, 'input.txt');
+
+let dataBoxIds = fs.readFileSync(filePath, {encoding: 'utf-8'});
+let boxIds = dataBoxIds.split('\n');
+let twoDuplicates = 0;
+let threeDuplicates = 0;
+
+for (i = 0; i < boxIds.length; i++) {
+  const strArr = boxIds[i].split("").sort().join("").match(/(.)\1+/g);
+
+  if (strArr != null) {
+    let has2 = strArr.some(function(elem) {
+      return elem.length == 2;
+    });
+    let has3 = strArr.some(function(elem) {
+      return elem.length == 3;
+    });
+    if (has2) {
+      twoDuplicates += 1;
+      console.log("Two Duplicates: " + twoDuplicates);
+    };
+    if (has3) {
+      threeDuplicates += 1;
+      console.log("Three Duplicates: " + threeDuplicates);
+    };
+  }
+}
+
